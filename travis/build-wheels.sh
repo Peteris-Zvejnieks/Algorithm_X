f854a24e1415697c215cb55ab1ab70b8cf7882c6
Pēteris Zvejnieks <peterf610@gmail.com>
2022-07-23 22:24:04 +0300

    manylinux

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -24,6 +24,6 @@ done
 # Install packages and test
 for PYBIN in /opt/python/*/bin/; do
     "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
-    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+    (cd "$HOME"; "${PYBIN}/pytests" /io/tests/*)
 done
 Footer
